72121b9b17fc49310a52354a1ecf3e065afb8832
Daniel Gazit <danieligazit@gmail.com>
2020-07-10 20:11:21 +0000

    Some tweaks

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 8081
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`App listening at port ${port}`))
