32ed7558ab503058ed37d2d9b030adbae502b66e
Daniel Gazit <danieligazit@gmail.com>
2020-07-11 18:02:08 +0000

    Trying react-dnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Card} from "./PlayingCard/PlayingCard";
+import { BrowserRouter as Router, Route, Link, DefaultRoute} from "react-router-dom";
 import './App.css';
+import { TouchBackend } from "react-dnd-touch-backend";
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const isTouchDevice = () => {
+  if ("ontouchstart" in window) {
+    return true;
+  }
+  return false;
+};
+
+// Assigning backend based on touch support on the device
+const backendForDND = isTouchDevice() ? TouchBackend : HTML5Backend;
+
+
+class App extends Component {
+  
+  render (){
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <DndProvider backend={backendForDND}>
+            <Card
+              value = {"1h"}
+              id = {"some_id"}
+              flippder = {false}
+              index = {0}
+              />
+            <Card
+              value = {"kh"}
+              id = {"some_id"}
+              flippder = {false}
+              index = {0}
+              />
+        
+     
+          </DndProvider>
+  
+           
+        </header>
+        
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
+
+     //   <PlayingCard
+          //         key={ "example" }
+          //         height={ 300 }
+          //         card={ "1h" }
+          //         flipped={ true }
+          //         elevateOnClick={true}
+          //         id={0}
+          //         index={0}
+          //         // onDragStart={this.onDragStart.bind(this)}
+          //         // onDragStop={this.onDragStop.bind(this)}
+          //         // onDrag={this.onDrag.bind(this)}
+          //         // onClick={this.onClick.bind(this)}
+          //     />
+          //   <PlayingCard
+          //         key={ "example" }
+          //         height={ 300 }
+          //         card={ "kh" }
+          //         flipped={ false }
+          //         elevateOnClick={true}
+          //         id={1}
+          //         index={0}
+          //         // onDragStart={this.onDragStart.bind(this)}
+          //         // onDragStop={this.onDragStop.bind(this)}
+          //         // onDrag={this.onDrag.bind(this)}
+          //     />
