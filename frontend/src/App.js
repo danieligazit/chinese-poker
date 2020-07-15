6e6b21ea7f08f2d8e2ab4ef632545b5b0a7612b1
Daniel Gazit <danieligazit@gmail.com>
2020-07-15 05:24:15 +0000

    create lobby button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,87 +1,63 @@
 import React from "react";
-import { Game } from "./Game"
-const util = require('util');
-const WebSocket = require('ws');
+import fetch from 'isomorphic-fetch';
+import ProgressButton from 'react-progress-button'
 
 const baseURL = "localhost:8081"
 
-class App extends React.Component {
+
+class CreateLobby extends React.Component {
+  getInitialState() {
+    return {
+      buttonState: '',
+      lobbyCreated: false
+    }
+  }
+  
   constructor(props) {
     super(props)
-    const ws = new WebSocket(`ws://${baseURL}/test?clientId=1`)
-    ws.binaryType = 'arraybuffer';
-    
-    ws.on('open', () => {
-      this.data = Buffer.from(JSON.parse({"actionType": "connect"}).data)
-      
-    })
-    
-    ws.on('message', (data) => {
-      var buf = new Uint8Array(data).buffer;
-      var dec = new util.TextDecoder("utf-8");
-      console.log(dec.decode(buf));
-    })
-    
-    this.game = React.createRef();
-  
-  // handleClick = () => {
-  //   this.game.current.setGameState({
-  //       hands: [
-  //         [
-  //           ["1h", "3s", "4s"],
-  //           ["Qs", "Kc"],
-  //           ["Td", "Qc"],
-  //           ["Jh", "3c", "Ts"],
-  //           ["8d", "1h"]
-  //         ],
-  //         [
-  //           ["nocard", "7s", "2h"],
-  //           ["7c", "Kd", "1c"],
-  //           ["nocard", "7d", "Qh"],
-  //           ["nocard", "Ts", "Jh"],
-  //           ["1c", "Qh", "Tc"]
-  //         ]
-  //       ],
-  //       isCurrentTurn: true,
-  //       topCard: "Qs",
-  //       iteration: 3,
-  //       playerIndex: 0
-      
-  //     }
-  //   )
-  //   setTimeout(() => {
-  //     console.log('slept')
-  //   this.game.current.setGameState({
-  //       hands: [
-  //         [
-  //           ["1h", "3s", "4s"],
-  //           ["Qs", "Kc", ],
-  //           ["Td", "Qc"],
-  //           ["Jh", "3c", "Ts"],
-  //           ["8d", "1h"]
-  //         ],
-  //         [
-  //           ["Js", "7s", "2h"],
-  //           ["7c", "Kd", "1c"],
-  //           ["nocard", "7d", "Qh"],
-  //           ["nocard", "Ts", "Jh"],
-  //           ["1c", "Qh", "Tc"]
-  //         ]
-  //       ],
-  //       isCurrentTurn: true,
-  //       topCard: "Qs",
-  //       iteration: 3,
-  //       playerIndex: 0
-      
-  //     }
-  //   )
-  //   }, 3000)
+    this.state = this.getInitialState()
   }
-  render(){
-    return(
-      <div onClick={this.handleClick}>
-        {this.data}
-        <Game ref={this.game}/>
+  
+  handleLobbyCreated() {
+    console.log('yay')  
+  }
+  
+  handleClick() {
+    if (this.state.lobbyCreated){ return this.handleLobbyCreated() }
+    
+    this.setState({buttonState: 'loading'})
+    
+    fetch(`https://${baseURL}/new/${this.gameSelection}`)
+      .then(response => response.json())
+      .then(data => this.setState({buttonState: 'success', lobbyCreated: true}))
+      .catch(() => this.setState({buttonState: 'error'}))
+  }
+  
+  render () {
+    return (
+      <ProgressButton onClick={this.handleClick.bind(this)} state={this.state.buttonState}>
+        CreateLobby
+      </ProgressButton>
+    )
+  }
+}
+
+class App extends React.Component {
+  
+  getInitialState() {
+    return {
+      gameSelection: "poker"
+    }
+  }
+  
+  handleClick(){
+    
+  }
+  
+  render() {
+    return (
+      <div>
+        <CreateLobby />
       </div>
     )
   }
---
+++ frontend/src/ChinesePoker.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { Game } from "./Game"
+const util = require('util');
+const WebSocket = require('ws');
+
+const baseURL = "localhost:8081"
+
+class ChinesePoker extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    const ws = new WebSocket(`ws://${props.baseURL}/${props.endpoint}?clientId=1`)
+    ws.binaryType = 'arraybuffer';
+    
+    ws.on('open', () => {
+      this.data = Buffer.from(JSON.parse({"actionType": "connect"}).data)
+      
+    })
+    
+    ws.on('message', (data) => {
+      var buf = new Uint8Array(data).buffer;
+      var dec = new util.TextDecoder("utf-8");
+      console.log(dec.decode(buf));
+    })
+    
+    this.game = React.createRef();
+  
+  // handleClick = () => {
+  //   this.game.current.setGameState({
+  //       hands: [
+  //         [
+  //           ["1h", "3s", "4s"],
+  //           ["Qs", "Kc"],
+  //           ["Td", "Qc"],
+  //           ["Jh", "3c", "Ts"],
+  //           ["8d", "1h"]
+  //         ],
+  //         [
+  //           ["nocard", "7s", "2h"],
+  //           ["7c", "Kd", "1c"],
+  //           ["nocard", "7d", "Qh"],
+  //           ["nocard", "Ts", "Jh"],
+  //           ["1c", "Qh", "Tc"]
+  //         ]
+  //       ],
+  //       isCurrentTurn: true,
+  //       topCard: "Qs",
+  //       iteration: 3,
+  //       playerIndex: 0
+      
+  //     }
+  //   )
+  //   setTimeout(() => {
+  //     console.log('slept')
+  //   this.game.current.setGameState({
+  //       hands: [
+  //         [
+  //           ["1h", "3s", "4s"],
+  //           ["Qs", "Kc", ],
+  //           ["Td", "Qc"],
+  //           ["Jh", "3c", "Ts"],
+  //           ["8d", "1h"]
+  //         ],
+  //         [
+  //           ["Js", "7s", "2h"],
+  //           ["7c", "Kd", "1c"],
+  //           ["nocard", "7d", "Qh"],
+  //           ["nocard", "Ts", "Jh"],
+  //           ["1c", "Qh", "Tc"]
+  //         ]
+  //       ],
+  //       isCurrentTurn: true,
+  //       topCard: "Qs",
+  //       iteration: 3,
+  //       playerIndex: 0
+      
+  //     }
+  //   )
+  //   }, 3000)
+  }
+  render(){
+    return(
+      <div onClick={this.handleClick}>
+        {this.data}
+        <Game ref={this.game}/>
+      </div>
+    )
+  }
+}
+
+
